f853550379e06b8d8f88e6629f9fb986077e0416
RamaSandhy <rshandy07@gmail.com>
2023-07-13 11:28:40 +0700

    Add project files.

---
+++ UASalgoritma/UASalgoritma.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//
+const int MAX_MAHASISWA = ;
+
+int NIM[MAX_MAHASISWA];
+string nama[MAX_MAHASISWA];
+int tahunMasuk[MAX_MAHASISWA];
+
+int jumlahMahasiswa();
+
+// 
+void tambahMahasiswa();
+this->mahasiswa = nilai;
+// 
+void tampilkanSemuaMahasiswa();
+this->NIMMAHASISWA
+//
+void algoritmacariMahasiswaByNIM();
+this->TahunMasuk
+//
+void algoritmaSortByTahunMasuk();
+
+int mian() {
+	int pilihan;
+	do {
+		cout << "========== MENU MANAJEMEN DATA MAHASISWA ==========" << endl;
+		cout << "1. Tambah Mahasiswa" << endl;
+		cout << "2. Tampilkan Semua Mahasiswa" << endl;
+		cout << "3. Cari Mahasiswa berdasarkan NIM" << endl;
+		cout << "4. Tampilkan Mahasiswa berdasarkan Tahun Masuk" << endl;
+		cout << "5. Keluar" << endl;
+		cin >> pilihan;
+		cin.ignore();
+
+		switch (pilihan) {
+		case 1:
+			"tambah mahasiswa" 
+
+			break;
+		case 2:
+			"Tampilkan Semua Mahasiswa"
+			//
+
+			break;
+		case 3:
+		    "Cari Mahasiswa berdasarkan NIM"
+			//
+
+			break
+		case 4:
+
+			// 
+
+			break;
+		case 5:
+			cout << "Terima kasih! Program selesai." << endl;
+			break;
+		default:
+			cout << "Pilihan tidak valid. Silahkan coba lagi." << endl;
+		}
+		cout << endl;
+
+	} while (pilihan != 5)
+	    while (true)
+		{
+			cout << "Enter the number of elements in the array: ";
+			cin >> n;
+			if ((n > 0) && (n <= 20))
+				break;
+			else
+				cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n"
+		}
+	return 0;
+}
+
+
+//1. menampilkan mengurutkan, dan mencari data mahasiswa dengan mudah
+//2. Algoritma logika dan menaklukan manusia
+//3. stack: LIFO
+     // queue: FIFO memiliki dua akses depan dan belakang.
+//4. 
